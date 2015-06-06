ff96676bdbf811b6e6530e847ee419462b50ea88
Jonathan Robinson <jayrowe86@gmail.com>
2015-06-05 23:48:32 -0500

    added twitter  embedded timeline

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -1,4 +1,18 @@
 @extends('layouts.default')
 @section('content')
-    <h1>Test</h1>
+    <div class="row">
+        <div class="col-md-6">
+            <h1>Twitter</h1>
+        </div>
+
+        <div class="col-md-12">
+            <a class="twitter-timeline" href="https://twitter.com/yoJayRowe" data-widget-id="607032484061011969">Tweets by @yoJayRowe</a>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+        </div>
+    </div>
+@stop
+
+@section('javascript')
+
 @stop
